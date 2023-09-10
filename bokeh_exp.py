9bb50c8f1420cd972af07991c5ae6deddf21405a
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-10 16:47:47 +0300

    Update bokeh_exp.py

--- bokeh_exp.py
+++ bokeh_exp.py
@@ -1,14 +1,50 @@
 import streamlit as st
+from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.plotting import figure
+import pandas as pd
+import numpy as np
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+@st.cache
+def data():
+    df = pd.DataFrame({"x": np.random.rand(500), "y": np.random.rand(500), "size": np.random.rand(500) * 10})
+    return df
 
-p = figure(
-    title='simple line example',
-    x_axis_label='x',
-    y_axis_label='y')
+df = data()
+source = ColumnDataSource(df)
 
-p.line(x, y, legend_label='Trend', line_width=2)
+st.subheader("Select Points From Map")
 
-st.bokeh_chart(p, use_container_width=True)
+plot = figure( tools="lasso_select,reset", width=250, height=250)
+plot.circle(x="x", y="y", size="size", source=source, alpha=0.6)
+
+source.selected.js_on_change(
+    "indices",
+    CustomJS(
+        args=dict(source=source),
+        code="""
+        document.dispatchEvent(
+            new CustomEvent("TestSelectEvent", {detail: {indices: cb_obj.indices}})
+        )
+    """,
+    ),
+)
+
+event_result = streamlit_bokeh_events(
+    events="TestSelectEvent",
+    bokeh_plot=plot,
+    key="foo",
+    debounce_time=100,
+    refresh_on_update=False
+)
+
+# some event was thrown
+if event_result is not None:
+    # TestSelectEvent was thrown
+    if "TestSelectEvent" in event_result:
+        st.subheader("Selected Points' Pandas Stat summary")
+        indices = event_result["TestSelectEvent"].get("indices", [])
+        st.table(df.iloc[indices].describe())
+
+st.subheader("Raw Event Data")
+st.write(event_result)
