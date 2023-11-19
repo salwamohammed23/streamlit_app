50ebce9bf1a5cd7ea2617a4fa820d9b48c1fd907
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-11-19 02:26:51 +0200

    Update t.py

--- t.py
+++ t.py
@@ -3,7 +3,6 @@ import requests
 from PIL import Image
 import io
 from transformers import pipeline
-import matplotlib.pyplot as plt
 
 API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
 HEADERS = {"Authorization": f"Bearer {'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'}"}
@@ -13,15 +12,14 @@ st.title("Image and Text Generation App")
 
 
 
-# Split the page into two columns
-col1, col2 = st.columns(2)
+# Split the page into two tabs
+tab1, tab2= st.tabs(["Image Generate App", "Text Generation App"])
 
 # Image Inference Section
-with col1:
-    st.header("Image Generation")
-    
+with tab1:
+    st.header("Image Inference")
     image_url = st.text_input("Enter your keyword:")
-    if st.button("Generate Image "):
+    if st.button("Generate Image"):
         # API Query
         response = requests.post(API_URL, headers=HEADERS, json={"inputs": image_url})
         image_bytes = response.content
@@ -31,15 +29,11 @@ with col1:
         st.image(image, caption="Inferred Image", use_column_width=True)
 
 # Text Generation Section
-with col2:
+with tab2:
     st.header("Text Generation")
- 
     keyword = st.text_input("Enter your keyword:")
     if st.button("Generate Text"):
         # Text Generation Pipeline
         generator = pipeline('text-generation', model='gpt2')
         generated_text = generator(keyword, max_length=100, num_return_sequences=1)[0]['generated_text']
         st.write(generated_text)
-
-
-
